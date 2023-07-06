7e1f7f36a003eb0fea6c01272fb45a26a5982f12
halasaielhaq <halasaielhaq@gmail.com>
2023-07-06 13:15:41 +0000

    T

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,44 +11,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-if (*s == 0)
-return (1);
-return (check_pal(s, 0, _strlen_recursion(s)));
-}
-
-/**
- *  _strlen_recursion - returns the length of a string
- *  @s: string to calculate the length of
- * 
- *  Return: length of the string
- */
-
-int _strlen_recursion(char *s)
-{
-if (*s == '\0')
-	return (0);
- return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
-{
-if (*(s + i) != *(s + len - 1))
-   return (0);
-if (i >= len)
-return (1);
-return (check_pal(s, i + 1, len - 1));
-}
-if (*s == 0)
-return (1);
-return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+        	return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -59,9 +24,9 @@ return (check_pal(s, 0, _strlen_recursion(s)));
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-	return (0);
-return (1 + _strlen_recursion(s + 1));
+        if (*s == '\0')
+	 	return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -74,9 +39,9 @@ return (1 + _strlen_recursion(s + 1));
  */
 int check_pal(char *s, int i, int len)
 {
-if (*(s + i) != *(s + len - 1))
-         return (0);
-if (i >= len)
-return (1);
-return (check_pal(s, i + 1, len - 1));
+         if (*(s + i) != *(s + len - 1))
+		return (0);
+	 if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
